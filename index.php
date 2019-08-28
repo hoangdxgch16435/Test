c3b5b1d1d9af48f9d4a7c76466a36b0a86448f37
hoangdxgch16435 <hoangdxgch16435@fpt.edu.vn>
2019-08-28 22:36:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="style.css" type="text/css">
-	<title>ATN SUPPER</title>
+	<title>H Toy Store</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link href="css/hover-min.css" rel="stylesheet">
